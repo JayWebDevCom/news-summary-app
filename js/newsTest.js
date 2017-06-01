639d63d3d348851a9a80a4da1f615aaedc2f167f
JayWebDevCom <jaiye.s@gmail.com>
2017-06-01 03:18:08 +0100

    add refactor so that responseText is set on open function of FakeXMLHttpRequest

--- js/newsTest.js
+++ js/newsTest.js
@@ -9,32 +9,38 @@ function testingTheTest() {
 
 function testingTheAPI() {
 
-function FakeXMLHttpRequest() {
-  this.readyState = 4
-  this.status = 200
-  this.responseText = JSON.stringify({
-    "response": {
-   "results": [
-      {
-         "webTitle":"England v South Africa: third ODI – live!",
-         "webUrl":"https://www.example.com"
-      },
-      {
-        "webTitle":"Jam review – tense classroom drama gives lessons in Britain's division",
-        "webUrl":"https://www.theguardian.com/stage/2017/may/29/jam-review-finborough-theatre"
-      }
-   ]
- }
+var responseText = JSON.stringify({
+  "response": {
+ "results": [
+    {
+       "webTitle":"England v South Africa: third ODI – live!",
+       "webUrl":"https://www.theguardian.com/stage/2017/may/29/this-is-a-mocked-url"
+    },
+    {
+      "webTitle":"Jam review – tense classroom drama gives lessons in Britain's division",
+      "webUrl":"https://www.theguardian.com/stage/2017/may/29/jam-review-finborough-theatre"
+    }
+ ]
+}
 })
+
+function FakeXMLHttpRequest() {
+  this.readyState = 0
+  this.status = 0
+  this.responseText = ''
+  this.onreadystatechange = function () {}
 }
 
 FakeXMLHttpRequest.prototype.open = function(){
-}
-FakeXMLHttpRequest.prototype.send = function(){
+  this.readyState = 4
+  this.status = 200
+  this.responseText = responseText
   this.onreadystatechange()
 }
 
-fakeAppElement = {}
+FakeXMLHttpRequest.prototype.send = function() {}
+
+var fakeAppElement = {}
 fakeAppElement.arrayOfItems = []
 
 fakeAppElement.appendChild = function(thing){
@@ -43,17 +49,21 @@ fakeAppElement.appendChild = function(thing){
 
 loadNews(api = "http://www.example.com",  XMLHttpRequestConstructor = FakeXMLHttpRequest, element = fakeAppElement)
 
-var url = JSON.parse(new FakeXMLHttpRequest().responseText).response.results[0].webUrl
-var title = JSON.parse(new FakeXMLHttpRequest().responseText).response.results[0].webTitle
+var sentHTTPRequest = new FakeXMLHttpRequest()
+sentHTTPRequest.open()
+var receivedResponseText = JSON.parse(sentHTTPRequest.responseText)
+
+var url = receivedResponseText.response.results[0].webUrl
+var title = receivedResponseText.response.results[0].webTitle
 var link1 = "<a href=\"" + url + "\">" + title + "</a>"
-var url2 = JSON.parse(new FakeXMLHttpRequest().responseText).response.results[1].webUrl
-var title2 = JSON.parse(new FakeXMLHttpRequest().responseText).response.results[1].webTitle
+var url2 = receivedResponseText.response.results[1].webUrl
+var title2 = receivedResponseText.response.results[1].webTitle
 var link2 = "<a href=\"" + url2 + "\">" + title2 + "</a>"
 
 var myArray = [fakeAppElement.arrayOfItems[0].innerHTML, fakeAppElement.arrayOfItems[1].innerHTML]
 
-var assert1 = new Assert(myArray[0], "API is functioning", [link1, link2][0])
-var assert2 = new Assert(myArray[1], "API is functioning", [link1, link2][1])
+var assert1 = new Assert(link1, "API is functioning for first response element", myArray[0])
+var assert2 = new Assert(link2, "API is functioning for second response element", myArray[1])
 
   try {
     assert1.isEqual()
